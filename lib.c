68a5a7351205099e600c6567f90050f5ba91f985
Nandakumar Edamana <nandakumar@nandakumar.co.in>
2025-08-13 15:43:27 +0530

    minor cleanup

--- lib.c
+++ lib.c
@@ -43,6 +43,8 @@ struct tnum tnum_mul(struct tnum a, struct tnum b)
 	return tnum_add(TNUM(acc_v, 0), acc_m);
 }
 
+// END from linux
+
 // TODO make sure this is correct
 struct tnum my_tnum_mul_proto(struct tnum a, struct tnum b)
 {
@@ -77,19 +79,19 @@ struct tnum my_tnum_mul(struct tnum a, struct tnum b)
 		/* LSB of tnum a is uncertain */
 		else if (a.mask & 1) {
 			// a.value[i] could be 0 or 1
-			struct tnum acc_m_0, acc_m_1;
+			struct tnum acc_0, acc_1;
 
 			// This approach considers mu's to be either zero or one altogether, solving the challenge pointed out by Harishankar et al. (see the last example in their paper -- 11 * x1)
 
 			// Assume a.value[i] is 0
-			acc_m_0 = tnum_add(acc, TNUM(0, 0)); // TODO simplify?
+			acc_0 = tnum_add(acc, TNUM(0, 0)); // TODO simplify?
 
 			// Assume a.value[i] is 1
 			u64 msk = b.value | b.mask;
 			struct tnum partprod = TNUM(b.value & ~msk, msk);
-			acc_m_1 = tnum_add(acc, partprod);
+			acc_1 = tnum_add(acc, partprod);
 
-			acc = tnum_union(acc_m_0, acc_m_1);
+			acc = tnum_union(acc_0, acc_1);
 		}
 		/* Note: no case for LSB is certain 0 */
 		a = tnum_rshift(a, 1);
@@ -97,8 +99,6 @@ struct tnum my_tnum_mul(struct tnum a, struct tnum b)
 	}
 	return acc;
 }
-
-// END from linux
 
 _Bool wellformed(struct tnum x)
 {
